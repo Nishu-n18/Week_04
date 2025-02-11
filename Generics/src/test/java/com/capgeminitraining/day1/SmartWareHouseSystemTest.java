c02f95195f39700e50cf73d5d07a4d9b31ecd9eb
Nishu-n18 <yrnishu03@gmail.com>
2025-02-11 13:45:17 +0530

    [Nishu] Added SmartWareHouseTest file

--- Generics/src/test/java/com/capgeminitraining/day1/SmartWareHouseSystemTest.java
+++ Generics/src/test/java/com/capgeminitraining/day1/SmartWareHouseSystemTest.java
@@ -18,6 +18,7 @@ public class SmartWareHouseSystemTest {
         assertEquals("Milk", milk.getName());
     }
 
+    
     @Test
     void testFurnitureCreation() {
         Furniture chair = new Furniture("Chair", "Wood");
