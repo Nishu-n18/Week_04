f4b6bcd61007258c3b127a06e823c08b9548cdc1
Nishu-n18 <yrnishu03@gmail.com>
2025-02-13 10:53:40 +0530

    [Nishu kumari] added Subset

--- Collections/src/main/java/com/capgeminitraining/day2/setinterface/SubsetChecker.java
+++ Collections/src/main/java/com/capgeminitraining/day2/setinterface/SubsetChecker.java
@@ -6,7 +6,6 @@ public class SubsetChecker {
     public static boolean isSubset(Set<Integer> subset, Set<Integer> superset) {
         return superset.containsAll(subset); // Check if superset contains all elements of subset
     }
-
     public static void main(String[] args) {
         Set<Integer> set1 = new HashSet<>(Arrays.asList(2, 3)); // Potential subset
         Set<Integer> set2 = new HashSet<>(Arrays.asList(1, 2, 3, 4)); // Superset
