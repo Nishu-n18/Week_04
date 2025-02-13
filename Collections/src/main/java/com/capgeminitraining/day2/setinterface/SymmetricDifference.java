f3e3f002a8baf2c5d8038a89108c696b12649da5
Nishu-n18 <yrnishu03@gmail.com>
2025-02-13 10:50:24 +0530

    [Nishu kumari] added Symmetric Difference

--- Collections/src/main/java/com/capgeminitraining/day2/setinterface/SymmetricDifference.java
+++ Collections/src/main/java/com/capgeminitraining/day2/setinterface/SymmetricDifference.java
@@ -13,7 +13,6 @@ public class SymmetricDifference {
         set.removeAll(intersection); // Remove common elements from union
         return set;
     }
-
     public static void main(String[] args) {
         Set<Integer> set1 = new HashSet<>(Arrays.asList(1, 2, 3));
         Set<Integer> set2 = new HashSet<>(Arrays.asList(3, 4, 5));
