9389b2c0441aa5154e807036c5b51ed5701b69a3
Nishu-n18 <yrnishu03@gmail.com>
2025-02-15 19:08:08 +0530

    [Nishu Kumari] added TaskManager

--- Day_05/src/main/java/com/capgeminitraining/day5/junit/TaskManager.java
+++ Day_05/src/main/java/com/capgeminitraining/day5/junit/TaskManager.java
@@ -4,6 +4,5 @@ public class TaskManager{
     public static String longRunningTask() throws InterruptedException {
         Thread.sleep(3000); // Simulates a long-running task (3 seconds)
         return "Task Completed";
-
     }
 }
