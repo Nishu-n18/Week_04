86c0ba56ebbcd3f69e00e230c94c32db09af9894
Nishu-n18 <yrnishu03@gmail.com>
2025-02-15 20:24:16 +0530

    [Nishu Kumari] added TemperatureConverter Test

--- Day_05/src/test/java/com/junittesting/TemperatureConverterTest.java
+++ Day_05/src/test/java/com/junittesting/TemperatureConverterTest.java
@@ -1,6 +1,6 @@
-package org.example.JunitQuestionsTest;
+package com.junittesting;
 
-import org.example.JunitQuestions.TemperatureConverter;
+import com.capgeminitraining.day5.junit.TemperatureConverter;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
