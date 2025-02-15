3e8cc9b92704eb42d22f298eeda63cfb94e84d4a
Nishu-n18 <yrnishu03@gmail.com>
2025-02-15 20:23:45 +0530

    [Nishu Kumari] added UserRegistrationTest

--- Day_05/src/test/java/com/junittesting/UserRegistrationTest.java
+++ Day_05/src/test/java/com/junittesting/UserRegistrationTest.java
@@ -1,6 +1,6 @@
-package org.example.JunitQuestionsTest;
+package com.junittesting;
 
-import org.example.JunitQuestions.UserRegistration;
+import com.capgeminitraining.day5.junit.UserRegistration;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
