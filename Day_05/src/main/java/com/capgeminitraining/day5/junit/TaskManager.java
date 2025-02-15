1ecb48069d0eca069ac5f64e5663a021b3919a2f
Nishu-n18 <yrnishu03@gmail.com>
2025-02-15 19:07:12 +0530

    [Nishu Kumari] added StringUtils

--- Day_05/src/main/java/com/capgeminitraining/day5/junit/TaskManager.java
+++ Day_05/src/main/java/com/capgeminitraining/day5/junit/TaskManager.java
@@ -4,6 +4,6 @@ public class TaskManager{
     public static String longRunningTask() throws InterruptedException {
         Thread.sleep(3000); // Simulates a long-running task (3 seconds)
         return "Task Completed";
-        
+
     }
 }
