45f1bed7d00203d06295daa0f6353a98395abb35
Nishu-n18 <yrnishu03@gmail.com>
2025-02-15 20:17:30 +0530

    [Nishu Kumari] added CalculatorTest

--- Day_05/src/test/java/com/junittesting/CalculatorTest.java
+++ Day_05/src/test/java/com/junittesting/CalculatorTest.java
@@ -1,6 +1,6 @@
-package org.example.JunitQuestionsTest;
+package com.junittesting;
 
-import org.example.JunitQuestions.Calculator;
+import com.capgeminitraining.day5.junit.Calculator;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
