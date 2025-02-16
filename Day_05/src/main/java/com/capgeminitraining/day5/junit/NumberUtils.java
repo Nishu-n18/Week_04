b73d4ada95f83e5f964ff99d7eb9e7652fd85f19
Nishu-n18 <yrnishu03@gmail.com>
2025-02-16 18:44:53 +0530

    [Nishu Kumari] added annotations and reflection

--- Day_05/src/main/java/com/capgeminitraining/day5/junit/NumberUtils.java
+++
@@ -1,7 +0,0 @@
-package com.capgeminitraining.day5.junit;
-
-public class NumberUtils {
-    public static boolean isEven(int number) {
-        return number % 2 == 0;
-    }
-}
--- Day_05/src/main/java/com/capgeminitraining/day5/junit/TaskManager.java
+++
@@ -1,8 +0,0 @@
-package com.capgeminitraining.day5.junit;
-
-public class TaskManager{
-    public static String longRunningTask() throws InterruptedException {
-        Thread.sleep(3000); // Simulates a long-running task (3 seconds)
-        return "Task Completed";
-    }
-}
